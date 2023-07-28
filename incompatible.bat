0a4b2ce6c1b7ccc1aaea4f168cb30f8defe31158
Cccc-owo <net4hhhh@163.com>
2023-07-28 21:30:50 +0800

    test: update incompatible.bat
    test carpet-fixes

--- incompatible.bat
+++ incompatible.bat
@@ -4,9 +4,11 @@ cd mods
 
 ::Below are the incompatible ( maybe ) mods. It should be listed by alphabet.
 ::=====BEGIN=====
+::carpet-fixes
 ::=====END=====
 
 ::Delete the incompatible ( maybe ) mods
+DEL /F /S /Q carpet-fixes.pw.toml
 
 ::Refresh packwiz index
 cd ..
