cda17eb7085c00e6d0272654561977bece60c0a6
Cccc-owo <net4hhhh@163.com>
2024-02-07 19:31:00 +0800

    feat: test mods

--- incompatible.bat
+++ incompatible.bat
@@ -4,11 +4,24 @@ cd mods
 
 ::Below are the incompatible ( maybe ) mods. It should be listed by alphabet.
 ::=====BEGIN=====
-::carpet-fixes
+::allow-portal-guis
+::ebe
+::iris
+::litematica
+::litemoretica
+::p2p
 ::=====END=====
 
 ::Delete the incompatible ( maybe ) mods
 ::DEL /F /S /Q carpet-fixes.pw.toml
+DEL /F /S /Q allow-portal-guis.pw.toml
+DEL /F /S /Q ebe.pw.toml
+DEL /F /S /Q iris.pw.toml
+DEL /F /S /Q litematica.pw.toml
+DEL /F /S /Q litemoretica.pw.toml
+DEL /F /S /Q p2p.pw.toml
+::Note:
+::- Wait for iris update
 
 ::Refresh packwiz index
 cd ..
