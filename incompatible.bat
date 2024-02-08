9f906c45e670a7a87eabf28e15c4e941d9af3a17
Cccc-owo <net4hhhh@163.com>
2024-02-08 13:27:16 +0800

    feat: fix bug mod

--- incompatible.bat
+++ incompatible.bat
@@ -3,25 +3,16 @@
 cd mods
 
 ::Below are the incompatible ( maybe ) mods. It should be listed by alphabet.
-::=====BEGIN=====
-::allow-portal-guis
-::ebe
-::iris
-::litematica
-::litemoretica
-::p2p
-::=====END=====
-
-::Delete the incompatible ( maybe ) mods
-::DEL /F /S /Q carpet-fixes.pw.toml
 DEL /F /S /Q allow-portal-guis.pw.toml
 DEL /F /S /Q ebe.pw.toml
+DEL /F /S /Q faster-random.pw.toml
 DEL /F /S /Q iris.pw.toml
 DEL /F /S /Q litematica.pw.toml
 DEL /F /S /Q litemoretica.pw.toml
 DEL /F /S /Q p2p.pw.toml
 ::Note:
 ::- Wait for iris update
+::- faster-random has problem
 
 ::Refresh packwiz index
 cd ..
