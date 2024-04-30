6f324f1b340109a31e8b703efa4032e438ef70d9
Cccc-owo <net4hhhh@163.com>
2024-04-30 10:20:24 +0800

    feat: remove 2 mods, add noisium

--- incompatible.bat
+++ incompatible.bat
@@ -3,10 +3,7 @@
 cd mods
 
 ::Below are the incompatible ( maybe ) mods. It should be listed by alphabet.
-DEL /F /S /Q allow-portal-guis.pw.toml
 DEL /F /S /Q ebe.pw.toml
-DEL /F /S /Q faster-random.pw.toml
-DEL /F /S /Q p2p.pw.toml
 ::Note:
 ::- Wait for iris update
 ::- faster-random has problem
