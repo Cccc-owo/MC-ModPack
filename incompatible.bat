c80cb24baafae7e75a133cca3a6f09060da0e39b
Cccc-owo <net4hhhh@163.com>
2024-06-24 20:16:58 +0800

    feat(mods): add cicada and fast-ip-ping, remove forgotten-graves and moddetectionpreventer
    - Add cicada and fast-ip-ping mods to the mods directory with their respective pw.toml files.
    - Remove forgotten-graves and moddetectionpreventer mods from the incompatible.bat list.
    - Update index.toml to include the new mods and remove the deprecated ones.
    - Bump pack version to 3.3.1 in pack.toml to reflect the changes.

--- incompatible.bat
+++ incompatible.bat
@@ -3,8 +3,14 @@
 cd mods
 
 ::Below are the incompatible ( maybe ) mods. It should be listed by alphabet.
+::DEL /F /S /Q mod.pw.toml
+attributefix.pw.toml
+effect-tooltips.pw.toml
+level-text-fix.pw.toml
+litemoretica.pw.toml
+max-health-fix.pw.toml
+nbt-autocomplete.pw.toml
 ::Note:
-::- Wait for iris update
 ::- faster-random has problem
 
 ::Refresh packwiz index
