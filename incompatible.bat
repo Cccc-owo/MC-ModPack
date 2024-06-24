d00c3df1e5413fc13eff3f57eb0116bb375676ef
Cccc-owo <net4hhhh@163.com>
2024-06-24 20:54:53 +0800

    chore(incompatible): update mod removal list and version bump
    - Add new mods to the incompatible.bat removal list with corresponding comments.
    - Remove the nbt-autocomplete.pw.toml entry as it's now compatible.
    - Bump pack version in pack.toml to 3.3.3 to track changes.

--- incompatible.bat
+++ incompatible.bat
@@ -5,11 +5,23 @@ cd mods
 ::Below are the incompatible ( maybe ) mods. It should be listed by alphabet.
 ::DEL /F /S /Q mod.pw.toml
 DEL /F /S /Q attributefix.pw.toml
+DEL /F /S /Q boathud.pw.toml @REM 1.20.6
+DEL /F /S /Q carpet.pw.toml @REM 1.20.6
+DEL /F /S /Q carpet-extra.pw.toml @REM 1.20.6
+DEL /F /S /Q carpet-fixes.pw.toml @REM 1.20.6
+DEL /F /S /Q chatanimation.pw.toml @REM 1.20.6
 DEL /F /S /Q effect-tooltips.pw.toml
+DEL /F /S /Q gamma-shifter.pw.toml @REM 1.20.6
+DEL /F /S /Q inventory-profiles-next.pw.toml @REM 1.20.6
+DEL /F /S /Q lambdynamiclights.pw.toml @REM 1.20.6
 DEL /F /S /Q level-text-fix.pw.toml
+DEL /F /S /Q libipn.pw.toml @REM 1.20.6
 DEL /F /S /Q litemoretica.pw.toml
+DEL /F /S /Q make_bubbles_pop.pw.toml @REM 1.20.6
 DEL /F /S /Q max-health-fix.pw.toml
-DEL /F /S /Q nbt-autocomplete.pw.toml
+DEL /F /S /Q nbt-autocomplete.pw.toml @REM 1.20.6
+DEL /F /S /Q staaaaaaaaaaaack.pw.toml @REM 1.20.6
+DEL /F /S /Q tape-stop.pw.toml @REM 1.20.6
 ::Note:
 ::- faster-random has problem
 
