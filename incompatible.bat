7269fa67958952944e3a253c02cad55b5bdc69ed
Cccc-owo <net4hhhh@163.com>
2024-07-02 15:32:56 +0800

    remove incompatible mods and update pack information

--- incompatible.bat
+++ incompatible.bat
@@ -3,10 +3,8 @@
 cd mods
 
 ::Below are the incompatible ( maybe ) mods. It should be listed by alphabet.
-DEL /F /S /Q ebe.pw.toml
+::DEL /F /S /Q mod.pw.toml
 ::Note:
-::- Wait for iris update
-::- faster-random has problem
 
 ::Refresh packwiz index
 cd ..
