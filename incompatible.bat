de5814544646856e744af96debf202612d64adaf
Cccc_ <me@iscccc.eu.org>
2024-09-13 17:23:13 +0800

    ci: update incompatible.bat

--- incompatible.bat
+++ incompatible.bat
@@ -6,16 +6,12 @@ cd mods
 ::DEL /F /S /Q mod.pw.toml
 DEL /F /S /Q boathud.pw.toml
 DEL /F /S /Q carpet-fixes.pw.toml
-DEL /F /S /Q carpet.pw.toml
-DEL /F /S /Q cit-resewn.pw.toml
 DEL /F /S /Q effect-tooltips.pw.toml
-DEL /F /S /Q fastquit.pw.toml
 DEL /F /S /Q lazydfu.pw.toml
 DEL /F /S /Q level-text-fix.pw.toml
 DEL /F /S /Q litemoretica.pw.toml
 DEL /F /S /Q max-health-fix.pw.toml
 DEL /F /S /Q memoryleakfix.pw.toml
-DEL /F /S /Q raised.pw.toml
 DEL /F /S /Q server-country-flags.pw.toml
 
 ::Note:
