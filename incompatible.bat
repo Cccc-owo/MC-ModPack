3b17d468a09ed2847ed55dda9f5916e77784da7b
Cccc_ <me@iscccc.eu.org>
2025-07-07 21:50:09 +0800

    feat: remove unused mods and add some replacements

--- incompatible.bat
+++ incompatible.bat
@@ -2,36 +2,16 @@ cd mods
 
 ::Below are the incompatible ( maybe ) mods. It should be listed by alphabet.
 ::DEL /F /S /Q mod.pw.toml
-DEL /F /S /Q boathud.pw.toml
-DEL /F /S /Q bounced.pw.toml
 DEL /F /S /Q carpet-fixes.pw.toml
 DEL /F /S /Q cit-resewn.pw.toml
-DEL /F /S /Q eating-animation.pw.toml
-DEL /F /S /Q effect-tooltips.pw.toml
 DEL /F /S /Q emi.pw.toml
-DEL /F /S /Q hold-that-chunk.pw.toml
-DEL /F /S /Q fabrishot.pw.toml
-DEL /F /S /Q faded-widgets.pw.toml
-DEL /F /S /Q flash.pw.toml
-DEL /F /S /Q flow.pw.toml
-DEL /F /S /Q it-follows!.pw.toml
-DEL /F /S /Q itemphysic-lite.pw.toml
 DEL /F /S /Q litemoretica.pw.toml
-DEL /F /S /Q max-health-fix.pw.toml
 DEL /F /S /Q memoryleakfix.pw.toml
 DEL /F /S /Q mixintrace.pw.toml
 ::DEL /F /S /Q moremcmeta.pw.toml
-::DEL /F /S /Q nbtedit-reborn.pw.toml
 DEL /F /S /Q nvidium.pw.toml
 ::DEL /F /S /Q obsidianui.pw.toml
-DEL /F /S /Q quick-menu.pw.toml
-DEL /F /S /Q ryoamiclights.pw.toml
-DEL /F /S /Q server-country-flags.pw.toml
-DEL /F /S /Q show-me-your-skin.pw.toml
-DEL /F /S /Q suggestion-tweaker.pw.toml
-DEL /F /S /Q third-person-maps.pw.toml
 ::DEL /F /S /Q tweakermore.pw.toml
-DEL /F /S /Q worldborderfix.pw.toml
 DEL /F /S /Q world-preview.pw.toml
 
 ::Note:
